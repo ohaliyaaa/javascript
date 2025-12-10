f37a1097345ca0ccaf234e1b157fb9da65422d2e
Aliya Khan <ohaliyaaakhan@gmail.com>
2025-12-10 21:07:55 +0000

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,5 +1,24 @@
 "use strict"; //treat all js code as newer version
+
 // this is a single line comment
 /* this is a multi-line comment*/
 
 // alert("hello") // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("aliya")
+
+console.log(3
+    +
+    3) // code readability should be high. this is not a good practice. avoid writing code like this. NOT READABLE
+
+// you can use mdn documentation
+// and for JS you can use ecma, tc39.es.
+
+let name = "Aliya";
+let age = 22;
+let isLoggedIn = true
+
+// datatypes
+// number 
+// string - single or double quotes
