9857eb17b61d6ffac71d4d346d865482800c8998
Aliya Khan <ohaliyaaakhan@gmail.com>
2025-12-10 20:05:39 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "ohaliya@google.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+let accountState;
+
+// accountId = 2222 // not allowed 
+
+accountEmail = "omg@google.com"
+accountPassword = "22889"
+accountCity = "Karachi"
+console.log(accountId); 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* PREFER NOT TO USE VAR
+BECAUSE OF ISSUE IN BLOCK SCOPE AND
+FUNCTIONAL SCOPE */
+
+
+
+
