1c08558e26787c64abab8aba8da8a4b0b7c390fb
Aliya Khan <ohaliyaaakhan@gmail.com>
2025-12-11 15:24:42 +0000

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -28,6 +28,8 @@ let x = 10; // number
 let y = "10"; //string
 let z; //undefined
 
+//typeof operator is used to check the datatype.
+
 console.log(typeof x);
 console.log(typeof y);
 console.log(typeof z);
