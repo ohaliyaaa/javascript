cf882e5ee62785198b080cb1a06bb6837d994da9
Aliya Khan <ohaliyaaakhan@gmail.com>
2026-01-21 20:05:55 +0000

    variables

--- basics/variables.js
+++ basics/variables.js
@@ -1,9 +1,16 @@
+// a variable is a container used to store data that can change later.
+// in javascript variables are declared using let and var (old one).
+
+// a constant stores data that cannot be changed once assigned.
+// in javascript, constants are declared using: const keyword.
+
 const accountId = 144553
 let accountEmail = "ohaliya@google.com"
 var accountPassword = "12345"
 accountCity = "Hyderabad"
 let accountState;
 
+// const is used to declare a variable that cannot be reassigned
 // accountId = 2222 // not allowed 
 
 accountEmail = "omg@google.com"
