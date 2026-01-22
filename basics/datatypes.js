a7f6d9f9e3022a7c84303bd7f3f5ddf0973499d7
Aliya Khan <ohaliyaaakhan@gmail.com>
2026-01-22 16:26:56 +0000

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,22 +1,26 @@
-"use strict"; //treat all js code as newer version
+"use strict"; // use strict treats all jvascript code as newer version 
 
 // this is a single line comment
 /* this is a multi-line comment*/
 
-// alert("hello") // we are using nodejs, not browser
+// alert (3 + 3) // we are using nodejs, not browser
 
-console.log(3+3)
-console.log("aliya")
+console.log(3+3); // this is a readable code
+console.log("aliya"); // this is also a readable code
 
 console.log(3
     +
     3) // code readability should be high. this is not a good practice. avoid writing code like this. NOT READABLE
 
-// you can use mdn documentation
-// and for JS you can use ecma, tc39.es.
+// ecmacript is the standard (rules) that define how javascript works (tc39.es).
+
+let name = "aliya khan";
+let age = 18;
+let isLoggedIn = true;
+let state;
 
 // list of primitive datatypes in javascript
-// number: 2 to the power of 53 is the range of number
+// number => 2 to power 53 is the range of number
 // bigInt: when the number is too large, used in trading websites
 // string: single or double quotes, preferable use double quotes
 // boolean: true to false
@@ -24,14 +28,29 @@ console.log(3
 // undefined: not assigned a value yet
 // symbol: unique
 
-let x = 10; // number
-let y = "10"; //string
-let z; //undefined
+// object
 
 //typeof operator is used to check the datatype.
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
 
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z);
+// the typeof null is object
+// the typeof undefined is undefined
+// the typeof name is string
+// the typeof age is number
+// the typeof state is undefined
+
+// primitive datatypes: primitive types are simple data values.
+// they store one value and are immutable (cannot be changed directly).
+
+// in javascript, primitive types are:
+// string, number, boolean, undefined, null, symbol, bogint
+
+// non-primitive datatypes: non-primitive data types are complex data types that can store multiple values and methods.
+// examples of non-primitive types in javascript: 
+// object, array, function, date, map/set
 
 
