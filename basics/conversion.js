bc4ae8b936e9ab12ab8fe717a7c6309870a95829
Aliya Khan <ohaliyaaakhan@gmail.com>
2026-01-22 17:41:18 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+let score = true
+
+console.log(typeof score);
+// u can also write this as console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(B)
