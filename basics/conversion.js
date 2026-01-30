c2173ed572ca7d5dcac3bf98b22f951123260953
Aliya Khan <ohaliyaaakhan@gmail.com>
2026-01-30 20:48:13 +0000

    conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,16 +1,33 @@
-let score = true
+let scoreOne = 33; // declared a variable named scoreOne with a value 33
+console.log(typeof scoreOne); // the typeof scoreOne is number
 
-console.log(typeof score);
-// u can also write this as console.log(typeof(score));
+let scoreTwo = "33"; // declared a variable named scoreTwo with a value "33"
+console.log(typeof scoreTwo); // the typeof scoreTwo is string
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+let valueInNumber = Number(scoreTwo); // declared another variable named as valueInNumber in which we converted string into number using Number and then the variable which needs to be converted.
+console.log(typeof valueInNumber); // the type in valueInNumber is number
+
+let scoreThree = undefined; // declared another variable as scoreThree with a value "33abc"
+let valueInNumber1 = Number(scoreThree); // // declared another variable named as valueInNumber1 in which we converted string into number using Number and then the variable which needs to be converted.
+console.log(typeof valueInNumber1); // the type in valueInNumber is number.
+console.log(valueInNumber1); // the value of valueInNumber1 is NaN.
 
 // "33" => 33
 // "33abc" => NaN
-// true => 1; false => 0
+// true => 1; false => 0;
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(B)
+let isLoggedIn = "aliya";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "aliya" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
