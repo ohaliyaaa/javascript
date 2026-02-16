61c6e5c9a9758c9b2a96279e715a4aa93f13d94c
Aliya Khan <ohaliyaaakhan@gmail.com>
2026-02-16 17:43:34 +0000

    operations

--- basics/operations.js
+++ basics/operations.js
@@ -2,70 +2,39 @@ let value = 3;
 let negValue = -value;
 console.log(negValue);
 
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
-console.log(2**2)
-console.log(2/2)
-console.log(2%2)
+console.log(2+2) // addition
+console.log(2-2) // subtraction
+console.log(2*2) // multiplication
+console.log(2**2) // power
+console.log(2/2) // division
+console.log(2%2) // remainder or modulus
 
 let str1 = "hello";
 let str2 = " i am aliya";
 let str3 = str1 + str2;
 console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
 
-console.log( (3 + 4) * 5 % 3);
-console.log(true);
-console.log(+true);
-console.log(+"");
+console.log( (3 + 4) * 5 % 3); // use parenthesis
+console.log(true); // it will give true
+console.log(+true); // confusing code, will give one
+// console.log(true+); // it will give error
+console.log(+""); // confusing code, will give zero
 
 let num1, num2, num3
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2 // there is no consistencey, bad code. readability
 
-let gameCounter = 100
-++gameCounter;
+let gameCounter = 100;
+gameCounter++; // post fix
 console.log(gameCounter);
 
-// ********************************************
-// we have two kinds of memories: stack and heap
-// stack is used with primitive data types
-// heap is used with non-primitive data types
-
-// stack copies the actual value.
-// heap copies the reference value.
-
-let myYoutubename = "aliyakhan";
-let anotherName = myYoutubename;
-anotherName = "ahmedali";
-
-console.log(anotherName);
-console.log(myYoutubename);
-
-let book = {
-  title: "Math",
-  available: true
-};
-
-let updatedBook = book;
-updatedBook.available = false;
-
-
-
-
-
-
-
-
-
-
-
-
-
+let gameCounter1 = 100;
+++gameCounter1; // pre fix
+console.log(gameCounter1);
 
 
 
