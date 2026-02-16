02dbd94b7105057eb471add3ee5215f5a318e7e3
Aliya Khan <ohaliyaaakhan@gmail.com>
2026-02-16 16:58:23 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // null converted into zero
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// comparison and equality check are different in javascript.
+// === -> strict check
+console.log(undefined === 0);
